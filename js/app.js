cacd0f0317c7dcb177aebabf68772cf69adf3562
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-03-23 00:39:01 +0100

    Created overlay function

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
+// Global Declaration
+const btnToggle = document.getElementById('btnMobileToggle');
+const body = document.body;
+
+
 // Get current year
 const currentYear = () => {
 	let d = new Date();
 	let year = d.getFullYear();
 	document.getElementById('currentYear').innerHTML = year;
 }
+
+// Set overlay
+const setOverlay = (value) =>{
+	let setValue = value;
+	console.log(setValue);
+	const overlay = document.querySelector('div.overlay');
+	if (setValue == true){
+		overlay.style.width = '100%';
+		overlay.scrollTop = 0;
+		body.classList.add('noScroll');
+	} else{
+		overlay.style.width = '0%';
+		body.classList.remove('noScroll');
+	}
+};
