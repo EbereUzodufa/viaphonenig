d4a3e08b32128dc5d58b5e63d8b5c0c6320b4593
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-04-02 16:04:19 +0100

    Created function to get json

--- js/app.js
+++ js/app.js
@@ -2,9 +2,38 @@
 const btnToggle = document.getElementById('btnMobileToggle');
 const body = document.body;
 let accordion = document.getElementsByClassName('cassavaApp-accordion');
-
 class DbHelper{
-	
+	static fetchJSONFromFile = (file) =>{
+		let arrValue = [];
+
+		function status(response) {
+		  if (response.status >= 200 && response.status < 300) {
+		    return Promise.resolve(response)
+		  } else {
+		    return Promise.reject(new Error(response.statusText))
+		  }
+		}
+
+		function json(response) {
+		  return response.json()
+		}
+
+		fetch(file)
+		  .then(status)
+		  .then(json)
+		  .then(function(data) {
+		  	const [values] = Object.values(data);
+		  	values.map(value => {
+		  		// jsonObj.push(data);
+		  		arrValue.push(value);
+		  	})
+		    console.log('Request succeeded with JSON response', data);
+		  }).catch(function(error) {
+		    console.log('Request failed', error);
+		  });
+
+	  	return arrValue;
+	}
 }
 
 // Get current year
