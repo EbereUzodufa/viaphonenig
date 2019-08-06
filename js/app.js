2a4ee2a241cd07d483e58ff815c53c4530c93082
Ebere Uzodufa <ebereuzodufa@gmail.com>
2019-08-06 11:39:47 +0100

    Disable service worker

--- js/app.js
+++ js/app.js
@@ -10,17 +10,18 @@ const btnProdToggle = document.getElementById('selectProduct');
 
 /*Adding service Worker*/
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('../sw.js')
-        .then(function(registration) {
-      		console.log('ServiceWorker registration successful');
-      	})
-    	.catch(function(err) {
-      		console.log('ServiceWorker registration failed: ' + err);
-		});
-    });
-};
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register('../sw.js')
+//         .then(function(registration) {
+//       		console.log('ServiceWorker registration successful');
+//       	})
+//     	.catch(function(err) {
+//       		console.log('ServiceWorker registration failed: ' + err);
+// 		});
+//     });
+// };
+// disabled
 
 const fetchJSONFromFile = (file, arrayEle) =>{
 	let arrValue = [];
